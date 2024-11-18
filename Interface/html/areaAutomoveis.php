bb4f7e5e02db3623747a60066120a53c88dd907f
Kauã Daniel Cardoso de Brito <kauandaniel2005@gmail.com>
2024-11-18 16:59:30 -0300

    FRONT-END

---
+++ Interface/html/areaAutomoveis.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title> Patio Automovéis </title>
+
+    <link rel="icon" type="image/x-icon" href="../images/icone.webp">
+    <link rel="stylesheet" href="../css/areaAutomoeis.css">
+</head> 
+<body>
+    <form method="POST" action="./telaVenda.php">
+        <div class="container">
+            <div class="box">
+                <div class="area" data-info="Informações da Area Alocada">Area 1 </div>
+                <div class="area" data-info="Informações da Area Alocada">Area 2 </div>
+                <div class="area" data-info="Informações da Area Alocada">Area 3 </div>
+                <div class="area" data-info="Informações da Area Alocada">Area 4 </div>
+                <div class="area" data-info="Informações da Area Alocada">Area 5 </div>
+                <div class="area" data-info="Informações da Area Alocada">Area 6 </div>
+                <div class="area" data-info="Informações da Area Alocada">Area 7 </div>
+                <div class="area" data-info="Informações da Area Alocada">Area 8 </div>
+                <div class="area" data-info="Informações da Area Alocada">Area 9 </div>
+                <div class="area" data-info="Informações da Area Alocada">Area 10 </div>
+                <!-- <div class="area" data-info="efgsdfgsgsdfgsdf"> 11 </div> -->
+            </div>
+            <div class="Area-Info" id="AreaInfo">
+                <label id="InfoAutomovel"> Informações</label>
+                <input type="submit" value="Vender" id="vender">
+            </div>
+        </div>
+    </form>
+    <script src="../js/janela.js"></script>
+</body>
+</html>
